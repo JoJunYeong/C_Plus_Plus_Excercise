fedd1f077109bada3f8052649e8465d57744469b
JoJunYeong <win414@hanmail.net>
2022-07-05 15:54:04 +0900

    VR Project init

---
+++ C#(VR_Unity)/My project/Assets/Coding.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coding : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string str = "Hello World!";
+        print(str);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
