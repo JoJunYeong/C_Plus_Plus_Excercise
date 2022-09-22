a608d73afdd2abf19bba3e7e15a10e799ebc0acf
조준영/연구원/BroadcastingJun.joMedia Unit(jun.jo@lge.com) <jun.jo@lge.com>
2022-09-22 17:42:27 +0900

    json 파일 조금 변경하고 디버그 단원까지 학습 완료 하지만 디버그 vs code에서 할 수 있도록 해놓는게 좋을 것 같다. 디버그 설정하다가 결국 못함.

--- C/study_files/14.c
+++ C/study_files/14.c
@@ -1,13 +1,15 @@
+/* 샘플 코드 */
 #include <stdio.h>
-int main() {
-  char a, b, c;
-  a = 100;
-  b = 300;
-  c = a + b;
 
-  printf("%d + %d = %d \n", a, b, c);
+int main() {
+  int arr[10];
+  int i;
+
+  for (i = 0; i < 10; i++) {
+    scanf("%d", &arr[i]);
+  }
+  for (i = 0; i <= 10; i++) {
+    printf("%d \n", arr[i]);
+  }
   return 0;
 }
-
-
-
