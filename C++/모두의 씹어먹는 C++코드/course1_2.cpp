1832ce86b1ca5bf6e188b3ba0d035e9d9af48952
조준영/연구원/BroadcastingJun.joMedia Unit(jun.jo@lge.com) <jun.jo@lge.com>
2022-12-07 09:40:06 +0900

    C++ 폴더트리 정리함

--- C++/모두의 씹어먹는 C++코드/course1_2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    
-    std::cout << "hi\nmy name is \nPsi" << std::endl;
-
-    return 0;
-}
--- C++/모두의 씹어먹는 C++코드/course1_problem_main.cpp
+++
@@ -1,9 +0,0 @@
-#include "course1_problem.h"
-
-int main(){
-    course1_problem course1;
-
-    std::cout << "Hello World!" << std::endl;
-
-    return 0;
-}
