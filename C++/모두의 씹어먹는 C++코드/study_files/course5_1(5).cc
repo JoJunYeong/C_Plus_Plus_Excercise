285d6baaec2a232c073dd0b8ce086c27257376dd
조준영/연구원/BroadcastingJun.joMedia Unit(jun.jo@lge.com) <jun.jo@lge.com>
2023-01-02 08:36:36 +0900

    2023 happy new year update

---
+++ C++/모두의 씹어먹는 C++코드/study_files/course5_1(5).cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+
+class MyString {
+    char *string_content;   // 문자열 데이터를 가리키는 포인터
+    int string_length;  // 문자열 길이
+
+    int memory_capacity; // 현재 할당된 용량
+
+    public:
+        // 문자 하나로 생성하는 생성자
+        MyString(char c);
+
+        // 문자열로 생성하는 생성자
+        MyString(const char* str);
+
+        // 복사 생성자
+        MyString(const MyString& str);
+
+        ~MyString();
+
+        int length();
+
+        void print() const;
+
+        void println() const;
+
+        char& operator[](const int index);
+
+
+};
+
+
+
+
+
+
+
+
